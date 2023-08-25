88cba35558ce6cc731cf9da4b37bf3542de7eb85
FrancoDellinocente <francodellinocente@gmail.com>
2023-08-25 17:31:41 -0300

    base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://francodellinocente.github.io/The_Met_Art_Explorer/'
+  base: '/The_Met_Art_Explorer/'
 })
